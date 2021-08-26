db0ce3db25504312ac85e7144359ca246381df8c
David Samson <david.andrew.engineer@gmail.com>
2021-08-25 22:52:05 -0400

    made videos rotated 90 degrees and added percentage complete on loading images

--- save_video.py
+++ save_video.py
@@ -3,33 +3,39 @@ import os
 import glob
 import imageio
 import re
+import numpy as np
 
 def main():
     folder_path = input('Enter path of folder with images to convert:\n')
     image_paths = []
     
-    print('Loading images...', end='')
-    sys.stdout.flush()
+    status = 'Loading images...'
+    print_percent(status)
+
     os.chdir(folder_path)
     for file in glob.glob('*.png'):
         image_paths.append(file)
     
     image_paths = natural_sort(image_paths)
-    images = [imageio.imread(image_path) for image_path in image_paths]
-    print('Done')
+    images = []
+    for i, image_path in enumerate(image_paths):
+        image = imageio.imread(image_path)
+        image = np.rot90(image)
+        images.append(image)
+        print_percent(status, i / len(image_paths) * 100)
+    print_percent(status)
+    print('Done    ')
 
     status = 'Writing images to video...'
-    sys.stdout.write(f'{status}\r')
-    sys.stdout.flush()
+    print_percent(status)
     fps = 60.0
     out = imageio.get_writer('timelapse.mp4', fps=fps)
     for i, image in enumerate(images):
         out.append_data(image)
-        percent = i / len(images) * 100
-        sys.stdout.write(f'{status}[{percent:.2f}%]\r')
-        sys.stdout.flush()
+        print_percent(status, i / len(images) * 100)
     out.close()
-    print(f'{status}Done')
+    print_percent(status)
+    print('Done    ')
 
 
 def natural_sort(l): 
@@ -37,6 +43,13 @@ def natural_sort(l):
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(l, key = alphanum_key)
 
+
+def print_percent(string, percent=None):
+    sys.stdout.write(f'\r{string}')
+    if percent is not None:
+        sys.stdout.write(f'[{percent:.2f}%]')
+    sys.stdout.flush()
+    pass
 
 
 if __name__ == '__main__':
