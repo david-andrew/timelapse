757ec40de8046e581908d12ffaaf290fe597c753
David Samson <david.andrew.engineer@gmail.com>
2021-11-15 18:20:34 -0500

    made save_video.py save while loading images rather than after loading images so that ram usage remains constant regardless of timelapse size

--- save_video.py
+++ save_video.py
@@ -4,47 +4,49 @@ import glob
 import imageio
 import re
 import numpy as np
+import time
 
 def main():
     folder_path = input('Enter path of folder with images to convert:\n')
-    out_path = 'output/timelapse.mp4'
+    out_path = f'output/timelapse_{time.time()}.mp4'
     image_paths = []
+    fps = 60.0
+    out = imageio.get_writer(out_path, fps=fps)
     
-    status = 'Loading images...'
+    status = 'Converting images to video...'
     print_percent(status)
 
     #save the starting path, and move to the path of the images
-    original_path = os.getcwd()
-    os.chdir(folder_path)
-
-    #load all the images (and apply a 90 deg rotation b/c my camera is sideways)
-    for file in glob.glob('*.png'):
-        image_paths.append(file)
+    #load all the images (and apply a 90 deg rotation b/c my camera is sideways) and write to video
+    # original_path = os.getcwd()
+    # os.chdir(folder_path)
+    # for file in glob.glob('*.png'):
+    #     image_paths.append(file)
+    # os.chdir(original_path)
+    image_paths = get_image_paths(folder_path)
     image_paths = natural_sort(image_paths)
-    images = []
+
     for i, image_path in enumerate(image_paths):
-        image = imageio.imread(image_path)
+        try:
+            image = imageio.imread(image_path)
+        except ValueError as e:
+            print(f'\nError loading image {image_path}')
+            print(e)
+            continue
         image = np.rot90(image)
-        images.append(image)
-        print_percent(status, i / len(image_paths) * 100)
-    print_percent(status)
-    print('Done     ')
-
-    #move back to the original directory to save the video
-    os.chdir(original_path)
-
-    #write the images to the output file
-    status = 'Writing images to video...'
-    print_percent(status)
-    fps = 60.0
-    out = imageio.get_writer(out_path, fps=fps)
-    for i, image in enumerate(images):
         out.append_data(image)
-        print_percent(status, i / len(images) * 100)
+
+        print_percent(status, i / len(image_paths) * 100)
     out.close()
     print_percent(status)
     print('Done     ')
 
+
+def get_image_paths(folder_path):
+    image_paths = []
+    for file in glob.glob(os.path.join(folder_path, '*.png')):
+        image_paths.append(file)
+    return image_paths
 
 def natural_sort(l): 
     convert = lambda text: int(text) if text.isdigit() else text.lower() 
